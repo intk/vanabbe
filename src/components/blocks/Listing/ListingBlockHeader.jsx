959a494ada7a75a4f380eeb56745760335ffcf89
kreafox <krisztina.n.elekes@gmail.com>
2022-12-21 12:19:27 +0200

    Fix listing headline

--- src/components/blocks/Listing/ListingBlockHeader.jsx
+++ src/components/blocks/Listing/ListingBlockHeader.jsx
@@ -1,12 +1,13 @@
 import { LinkMore } from '@plone/volto/components';
 
 const ListingBlockHeader = ({ data, children, sliderView }) => {
-  const { title, headline, linkHref } = data;
+  const { title, headline, linkHref, headlineTag } = data;
   const head = title || headline;
+  const CustomTag = `${headlineTag || 'h2'}`;
 
   return head ? (
     <div className="listing-block-header">
-      <h2>{head}</h2>
+      {headline && <CustomTag>{headline}</CustomTag>}
       {!sliderView && linkHref ? <LinkMore data={data} /> : ''}
       {children}
     </div>
