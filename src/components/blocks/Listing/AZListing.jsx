dde6f4a28b859d6e5a81086dd0fafd9415185d49
kreafox <krisztina.n.elekes@gmail.com>
2023-01-31 15:18:51 +0200

    A-Z listing view styling

--- src/components/blocks/Listing/AZListing.jsx
+++ src/components/blocks/Listing/AZListing.jsx
@@ -1,6 +1,8 @@
 import { UniversalLink } from '@plone/volto/components';
 import { ListingBlockHeader } from '@package/components';
 
+import './less/az-listing.less';
+
 const AgendaListingTemplate = (data) => {
   const { items = [], linkHref } = data;
   const groups = items.reduce(
@@ -24,16 +26,15 @@ const AgendaListingTemplate = (data) => {
             {data.linkTitle || '...'}
           </UniversalLink>
         )}
-
-        <div className="az-listing-nav">
-          {Object.keys(groups)
-            .sort()
-            .map((letter) => (
-              <a key={letter} href={`#g-${letter}`}>
-                {letter}
-              </a>
-            ))}
-        </div>
+      </div>
+      <div className="az-listing-nav">
+        {Object.keys(groups)
+          .sort()
+          .map((letter) => (
+            <a key={letter} href={`#g-${letter}`}>
+              {letter}
+            </a>
+          ))}
       </div>
 
       <div className="az-listing-content">
@@ -41,8 +42,14 @@ const AgendaListingTemplate = (data) => {
           .sort()
           .map((letter) => (
             <div key={letter}>
-              <h4 id={`g-${letter}`}>{letter}</h4>
-              <ul>
+              <h4 className="letter" id={`g-${letter}`}>
+                {letter}
+              </h4>
+              <ul
+                className={`az-list ${
+                  groups[letter].length > 6 ? 'multi-column' : 'single-column'
+                }`}
+              >
                 {groups[letter].map((item) => (
                   <li key={item['@id']}>
                     <UniversalLink item={item}>{item.title}</UniversalLink>
