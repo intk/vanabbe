a5065ddbea08de466fe445a86a758662c5f80c04
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-02-02 17:28:25 +0200

    Don't load search route

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,18 @@
  */
 
 // import { App } from '@plone/volto/components';
+
+// import { App } from '@plone/volto/components';
+// import { defaultRoutes } from '@plone/volto/routes';
+// import config from '@plone/volto/registry';
 import routes from '@plone/volto/routes';
 // import config from '@plone/volto/registry';
+
+routes[0].routes = routes[0].routes
+  .filter((r) => r.path !== '/search')
+  .filter((r) => r.path !== '/(nl|en)/search');
+
+console.log('routes', routes);
 
 /**
  * Routes array.
@@ -43,5 +53,22 @@ import routes from '@plone/volto/routes';
 //
 //
 // console.log('routes', routes);
+
+/**
+ * Routes array.
+ * @array
+ * @returns {array} Routes.
+ */
+// const routes = [
+//   {
+//     path: '/',
+//     component: App, // Change this if you want a different component
+//     routes: [
+//       // Add your routes here
+//       ...(config.addonRoutes || []),
+//       ...defaultRoutes.filter(({ path }) => path !== '/search'),
+//     ],
+//   },
+// ];
 
 export default routes;
