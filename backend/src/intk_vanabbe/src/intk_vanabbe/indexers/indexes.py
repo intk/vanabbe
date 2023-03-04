8ec6583394a78cff17f32e734fa934d7d7d6528d
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-03-04 16:45:10 +0200

    Add decade indexer

--- backend/src/intk_vanabbe/src/intk_vanabbe/indexers/indexes.py
+++ backend/src/intk_vanabbe/src/intk_vanabbe/indexers/indexes.py
@@ -1,3 +1,5 @@
+from .utils import decade
+from intk_vanabbe.content.artwork import IArtwork
 from intk_vanabbe.content.exhibition import IExhibition
 from intk_vanabbe.content.publication import IPublication
 from plone.indexer.decorator import indexer
@@ -25,3 +27,12 @@ def exhibition_description(obj):
 @indexer(IPublication)
 def author_name(obj):
     return obj.bookauthorName
+
+
+@indexer(IArtwork)
+def artwork_decade(obj):
+    created = getattr(obj, "objectCreationDate", None)
+    from_ = getattr(obj, "objectCreationDateFrom", None)
+    to_ = getattr(obj, "objectCreationDateTo", None)
+
+    return [d for d in set([decade(created), decade(from_), decade(to_)]) if d]
