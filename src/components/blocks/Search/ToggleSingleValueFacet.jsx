d105f1e63d332ec65489bbc954d6f1f878f81ffe
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-03-20 20:38:17 +0200

    Show author in facet filter

--- src/components/blocks/Search/ToggleSingleValueFacet.jsx
+++ src/components/blocks/Search/ToggleSingleValueFacet.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Checkbox, Header } from 'semantic-ui-react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getQueryStringResults } from '@plone/volto/actions';
 
 const ToggleSingleValueFacet = (props) => {
   const { facet, isEditMode, onChange, value } = props; // value, choices, isMulti, onChange,
-  const label = value.value; // TODO: what to do about this?
+  const { label = value.value } = value; // TODO: what to do about this?
 
   return (
     <div className="button-facet">
@@ -40,4 +42,54 @@ ToggleSingleValueFacet.valueToQuery = ({ value, facet }) => {
     : null;
 };
 
-export default ToggleSingleValueFacet;
+const withAuthor = (WrappedComponent) => {
+  function WithAuthor(props) {
+    const { value, facet } = props;
+    const authorID = value.value;
+
+    const dispatch = useDispatch();
+    const [author, setAuthor] = React.useState();
+    const currentLang = useSelector((state) => state.intl.locale);
+    const isAuthorField = facet.field.value === 'authorID';
+
+    React.useEffect(() => {
+      if (isAuthorField && !author) {
+        dispatch(
+          getQueryStringResults(
+            '/',
+            {
+              query: [
+                {
+                  i: 'portal_type',
+                  o: 'plone.app.querystring.operation.selection.any',
+                  v: ['author'],
+                },
+                {
+                  i: 'authorID',
+                  o: 'plone.app.querystring.operation.selection.is',
+                  v: authorID,
+                },
+                {
+                  i: 'Language',
+                  o: 'plone.app.querystring.operation.selection.is',
+                  v: currentLang,
+                },
+              ],
+            },
+            `content-${authorID}`,
+          ),
+        ).then((results) => {
+          const { items } = results || {};
+          if (items.length) {
+            setAuthor(items[0].title);
+          }
+        });
+      }
+    }, [isAuthorField, authorID, author, dispatch, currentLang]);
+
+    return <WrappedComponent {...props} value={{ ...value, label: author }} />;
+  }
+  return WithAuthor;
+};
+
+export default withAuthor(ToggleSingleValueFacet);
