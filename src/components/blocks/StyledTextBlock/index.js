1eed9007624d808872d732364589e728fd7eeb43
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-03-30 14:05:37 +0300

    Add artwork_details in site data

--- src/components/blocks/StyledTextBlock/index.js
+++ src/components/blocks/StyledTextBlock/index.js
@@ -1,12 +1,13 @@
 import codeSVG from '@plone/volto/icons/row.svg';
-import StyledBlockView, { SmallText } from './StyledTextBlockView';
+import StyledBlockView, { SmallText, NormalText } from './StyledTextBlockView';
 import StyledBlockEdit from './StyledTextBlockEdit';
 import Schema from './schema';
+import { cloneDeep } from 'lodash';
 
 export default function applyConfig(config) {
   config.blocks.blocksConfig.styledText = {
     id: 'styledText',
-    title: 'Small text',
+    title: 'Text section',
     icon: codeSVG,
     group: 'common',
     view: StyledBlockView,
@@ -24,9 +25,29 @@ export default function applyConfig(config) {
       {
         id: 'default',
         isDefault: true,
-        title: 'Default',
+        title: 'Small text',
         template: SmallText,
         schemaEnhancer: ({ schema }) => ({ ...schema, title: 'Small text' }),
+      },
+      {
+        id: 'normal',
+        isDefault: false,
+        title: 'Normal text',
+        template: NormalText,
+        schemaEnhancer: ({ schema }) => {
+          schema = cloneDeep(schema);
+          schema.title = 'Normal text';
+          schema.fieldsets[0].fields = ['globalId'];
+          schema.properties.globalId = {
+            title: 'Global ID',
+            description: 'Identify this block for templating purposes',
+            // TODO: filter the available ids based on existing blocks in data
+            choices: [['artwork_details', 'Artwork details']],
+            default: 'artwork_details',
+          };
+
+          return schema;
+        },
       },
     ],
   };
