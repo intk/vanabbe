2771a2c76463cca066d213612f73d053a24ecfca
Cihan Andac <cihanandac@gmail.com>
2023-10-04 13:33:29 +0200

    chores

--- backend/src/intk_vanabbe/src/intk_vanabbe/browser/admin.py
+++ backend/src/intk_vanabbe/src/intk_vanabbe/browser/admin.py
@@ -685,7 +685,7 @@ class AdminFixes(BrowserView):
                         if v:
                             setattr(obj, k, json.dumps(v))
 
-                    print(f"Updated Object ID: {obj.getId()}, Path: {obj.absolute_url()}, Workflow State: {api.content.get_state(obj)}")
+                    # print(f"Updated Object ID: {obj.getId()}, Path: {obj.absolute_url()}, Workflow State: {api.content.get_state(obj)}")
                     
                     if lang == "nl":
                         for author in authors:
@@ -718,11 +718,22 @@ class AdminFixes(BrowserView):
             # Linking two objects as translations of each other
             brains = catalog.searchResults(ccObjectID=ccObjectID, portal_type="artwork")
             if len(brains)>1:
-                obj = brains[0].getObject()
-                obj_en = brains[1].getObject()
-                manager = ITranslationManager(obj)
-                if not manager.has_translation('en'):
-                    manager.register_translation('en', obj_en)
+                # Split the brains into two based on language
+                obj = None
+                obj_en = None
+                
+                for brain in brains:
+                    current_obj = brain.getObject()
+                    if current_obj.Language() == 'en':
+                        obj_en = current_obj
+                    else:
+                        obj = current_obj
+
+                # Check if both objects were identified correctly
+                if obj and obj_en:
+                    manager = ITranslationManager(obj)
+                    if not manager.has_translation('en'):
+                        manager.register_translation('en', obj_en)
 
             #adding images
             images=element.xpath(f"//dc_record/objectImage")
