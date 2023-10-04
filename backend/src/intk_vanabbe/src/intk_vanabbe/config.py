1463c18d100922f317617a1e99e50c44106b4747
Cihan Andac <cihanandac@gmail.com>
2023-10-04 21:42:44 +0200

    test for new location of artist

--- backend/src/intk_vanabbe/src/intk_vanabbe/config.py
+++ backend/src/intk_vanabbe/src/intk_vanabbe/config.py
@@ -1,14 +1,14 @@
 DATA_REPO = "/Users/cihanandac/Documents/vanabbe/data-import"
 
 IMPORT_LOCATIONS = {
-    "artwork": "nl/collectie-x",
-    "artwork_en": "en/collection-x",
+    "artwork": "nl/collectie",
+    "artwork_en": "en/collection",
     "publication": "nl/collectie-onderzoek/bibliotheek/publicaties",
     "exhibition": "nl/tentoonstellingen",
-    "author": "nl/collectie-onderzoek/kunstenaars",
-    "author_en": "en/collection-research/artists"
-    # "author": "nl/kunstenaars",
-    # "author_en": "en/artists",
+    # "author": "nl/collectie-onderzoek/kunstenaars",
+    # "author_en": "en/collection-research/artists"
+    "author": "nl/kunstenaars",
+    "author_en": "en/artists",
 }
 
 # IMAGE_BASE_URL = "https://vanabbemuseum.nl/fileadmin/files/collectie/%s"
