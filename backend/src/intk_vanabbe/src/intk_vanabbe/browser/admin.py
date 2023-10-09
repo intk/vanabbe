991f738af2681b36db1700e40e886d72b1461469
Cihan Andac <cihanandac@gmail.com>
2023-10-10 00:11:50 +0200

    Fix the already existed and half deleted objects

--- backend/src/intk_vanabbe/src/intk_vanabbe/browser/admin.py
+++ backend/src/intk_vanabbe/src/intk_vanabbe/browser/admin.py
@@ -681,19 +681,48 @@ class AdminFixes(BrowserView):
                 lang = brains[0].getObject().language
                 missing_lang = 'en' if lang == 'nl' else 'nl'
                 if missing_lang == 'nl':
-                    obj = create_and_setup_object(title, container, info) #Dutch version
+                    obj = create_and_setup_object(title, container, info, intl) #Dutch version
                     log_to_file(f"{ccObjectID} Dutch version of object is created")
                     for author in authors:
                         relation.create(source=obj, target=author, relationship="authors")
+                    
+                    manager = ITranslationManager(obj)
+                    if not manager.has_translation('en'):
+                        manager.register_translation('en', brains[0].getObject())
+                    
+                    #adding images
+                    images=element.xpath(f"//dc_record/objectImage")
+                    if images:
+                        import_images(
+                            container= obj, 
+                            images=images
+                            )
+                        obj.hasImage=True;
+                    
+                    
+
                 else:
-                    obj_en = create_and_setup_object(title, container_en, info) #English version
+                    obj_en = create_and_setup_object(title, container_en, info, intl) #English version
                     log_to_file(f"{ccObjectID} English version of object is created")
                     for author_en in authors_en:
                         relation.create(source=obj_en, target=author_en, relationship="authors")
 
+                    manager = ITranslationManager(obj_en)
+                    if not manager.has_translation('nl'):
+                        manager.register_translation('nl', brains[0].getObject())
+                    
+                    #adding images
+                    images=element.xpath(f"//dc_record/objectImage")
+                    if images:
+                        import_images(
+                            container= obj_en, 
+                            images=images
+                            )
+                        obj_en.hasImage=True;
+                    
             # Check if object with ccObjectID already exists in the container
-            brains = catalog.searchResults(ccObjectID=ccObjectID)
-            if brains:
+            # brains = catalog.searchResults(ccObjectID=ccObjectID)
+            elif brains:
                 for brain in brains:
                     # Object exists, so we fetch it and update it
                     obj = brain.getObject()
@@ -722,6 +751,15 @@ class AdminFixes(BrowserView):
                     
                     log_to_file(f"{ccObjectID} object is updated")
 
+                    #adding images
+                    images=element.xpath(f"//dc_record/objectImage")
+                    if images:
+                        import_images(
+                            container= obj, 
+                            images=images
+                            )
+                    obj.hasImage=True;
+
                     # Reindex the updated object
                     obj.reindexObject()
                     obj.reindexObject(idxs=['objectTitle', 'Title', 'sortable_title', 'authorID'])
@@ -743,26 +781,17 @@ class AdminFixes(BrowserView):
                     relation.create(source=obj_en, target=author_en, relationship="authors")
 
                 logger.info("Created %s", obj.absolute_url(relative=1))        
-            
-            # # Linking two objects as translations of each other
-            # brains = catalog.searchResults(ccObjectID=ccObjectID)
-            # if len(brains)>1:
-            #     obj = brains[0].getObject()
-            #     obj_en = brains[1].getObject()
-            #     manager = ITranslationManager(obj)
-            #     if not manager.has_translation('en'):
-            #         manager.register_translation('en', obj_en)
 
-            #adding images
-            images=element.xpath(f"//dc_record/objectImage")
-            if images:
-                import_images(
-                    container= obj, 
-                    images=images
-                    )
-                obj.hasImage=True;
-            
-            obj_en = self.translate(obj, info['en'])
+                #adding images
+                images=element.xpath(f"//dc_record/objectImage")
+                if images:
+                    import_images(
+                        container= obj, 
+                        images=images
+                        )
+                    obj.hasImage=True;
+                
+                obj_en = self.translate(obj, info['en'])
             
             counter += 1
 
