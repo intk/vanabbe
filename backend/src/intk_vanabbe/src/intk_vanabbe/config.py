47f2a6736eb20e1ec2fc88dc57ce3992aa3f433e
Cihan Andac <cihanandac@gmail.com>
2023-10-12 14:05:26 +0200

    Add exhibition_en location

--- backend/src/intk_vanabbe/src/intk_vanabbe/config.py
+++ backend/src/intk_vanabbe/src/intk_vanabbe/config.py
@@ -5,6 +5,7 @@ IMPORT_LOCATIONS = {
     "artwork_en": "en/collection",
     "publication": "nl/collectie-onderzoek/bibliotheek/publicaties",
     "exhibition": "nl/tentoonstellingen",
+    "exhibition_en": "en/exhibitions",
     "author": "nl/collectie-onderzoek/kunstenaars",
     "author_en": "en/collection-research/artists"
     # "author": "nl/kunstenaars",
