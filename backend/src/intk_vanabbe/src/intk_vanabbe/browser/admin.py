bf1d7ae52898ed1e48a14e2bf1b56658b589018f
Cihan Andac <cihanandac@gmail.com>
2024-02-21 12:37:22 +0100

    chores

--- backend/src/intk_vanabbe/src/intk_vanabbe/browser/admin.py
+++ backend/src/intk_vanabbe/src/intk_vanabbe/browser/admin.py
@@ -1470,8 +1470,8 @@ class AdminFixes(BrowserView):
         log_to_file(f"Starting the sync function for the date after {date_from}")
         log_to_file(f"total count of objects for update = {total_count}")
         for offset in range(int(start_range), int(total_count), 500):
+            transaction.begin()
             self.sync_new_objects(start_range=offset, end_range=offset+500, date_from=date_from)
-
             transaction.commit()
         
         return "all done"
